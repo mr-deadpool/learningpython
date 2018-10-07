1ee1d1ee623caaa0c59b45cdd10cd29aa2af74da
Adithya Shukla <sunny@Adithyas-MBP.attlocal.net>
2018-10-07 13:39:41 -0700

    commiting first file

---
+++ character input.py
@@ -0,0 +1,10 @@
+name = raw_input("Enter your name:")
+
+print "Your name is " + name
+
+age = int(raw_input("How old are you?:"))
+
+year = str((2018 - age)+100)
+
+print "You " + name + " will be 100 years old on " + year
+
